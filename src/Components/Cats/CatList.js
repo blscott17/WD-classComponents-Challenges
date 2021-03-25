8037295cabca0cb7faa011ac32cd28eab42b1d23
Bonita Scott <bonitalscott@yahoo.com>
2021-03-25 10:25:31 -0400

    Cat Challenge Solution

--- src/Components/Cats/CatList.js
+++ src/Components/Cats/CatList.js
@@ -1,11 +1,45 @@
-import React, { Component } from 'react';
- 
-const CatList = () => {
- return (
-   <div>
-     {props.breeds.Map(cat => <li>cat</li> )}
-   </div>
- )
-}
- 
-CatList;
+import React from 'react';
+
+const CatList = (props) => {
+  return (
+    <ul>
+      {props.breeds.map((cats, index) => (
+        <li key={index}>{cats}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default CatList;
+
+// import React, { Component } from 'react';
+
+// class CatList extends Component {
+//   render() {
+//     const breeds = () => this.props.cats.map((cat) => <li>{cat}</li>);
+//     return (
+//       <div>
+//         <h2>List of Cat Breeds</h2>
+//         <ul>{breeds()}</ul>
+//       </div>
+//     );
+//   }
+// }
+
+// export default CatList;
+
+// import React, { Component } from 'react';
+
+// class CatList extends Component {
+//   render() {
+//     const breeds = () => this.props.cats.map((cat) => <li>{cat}</li>);
+//     return (
+//       <div>
+//         <h2>List of Cat Breeds</h2>
+//         <ul>{breeds()}</ul>
+//       </div>
+//     );
+//   }
+// }
+
+// export default CatList;
